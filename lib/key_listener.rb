bb3fbfec7c7cc57f659f6c710d139e27b0726664
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-09-26 21:35:32 -0400

    key listener added

---
+++ lib/key_listener.rb
@@ -0,0 +1,30 @@
+require 'socket'
+
+Thread.abort_on_exception = true
+
+class KeyListener
+  def initialize port, callback
+    @port = port
+    @callback = callback
+    listen
+  end
+
+  def listen
+    Thread.new do
+      Socket.tcp_server_loop @port, &register_listener
+    end
+  end
+  private :listen
+
+  def register_listener
+    ->(raw_socket, client_info) {
+      Thread.new do
+        key = raw_socket.read
+        @callback[key]
+        raw_socket.close
+      end
+    }
+  end
+  private :register_listener
+
+end
---
+++ spec/key_listener_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../lib/key_listener'
+
+describe KeyListener do
+  context "calls update proc when key are recived" do
+    after do
+      Thread.new do
+        listener
+      end
+      Socket.tcp('localhost', port) {|client|
+        client.write message
+        client.close_write
+      }
+    end
+    
+    let(:port)     { 2003 }
+    let(:callback) { ->(key){} }
+    let(:listener) { KeyListener.new port, callback }
+    let(:message)  { "j" }
+    
+    subject { callback }
+    it { should receive(:call).with(message) }
+  end
+end
