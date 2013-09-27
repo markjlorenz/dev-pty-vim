b6d35f187cb8d78df19623f82ca5f07051e1e091
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-09-26 22:37:28 -0400

    get access to clients

--- lib/pty_manager.rb
+++ lib/pty_manager.rb
@@ -7,14 +7,16 @@ require_relative 'registration_server'
 
 class PtyManager
   def initialize(registration_port, key_port, key_file, vim_rc)
-    @pty_m, @pty_s     = PTY.open
-    @key_file          = key_file
-    @vim_rc_path       = vim_rc
-    @vim_rc            = File.new(vim_rc)
+    @pty_m, @pty_s       = PTY.open
+    @key_file            = key_file
+    @vim_rc_path         = vim_rc
+    @vim_rc              = File.new(vim_rc)
+    @registration_server = RegistrationServer.new(registration_port, @vim_rc)
+    @key_listener        = KeyListener.new(key_port, remote_key_callback)
 
     start_key_control_loop
-    RegistrationServer.new(registration_port, @vim_rc).start
-    KeyListener.new(key_port, remote_key_callback).start
+    @registration_server.start
+    @key_listener.start
   end
 
   def start_key_control_loop
@@ -30,18 +32,19 @@ class PtyManager
 
   def remote_key_callback
     ->(key){
-      save_key  char
-      @pty_m << char
+      save_key  key
+      @pty_m << key
     }
   end
 
   def save_key key
-    File.open(@key_file, 'a') { |f| f.putc char }
+    File.open(@key_file, 'a') { |f| f.putc key }
   end
   private :save_key
 
   def send_key key
-    KeySender.new clients, char
+    clients = @registration_server.clients
+    KeySender.new clients, key
   end
   private :send_key
 
