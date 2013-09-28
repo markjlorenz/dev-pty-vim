ec02e956409bacdcb4ea909fad463a185128464e
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-09-28 11:16:15 -0400

    plea for help

--- spec/key_listener_spec.rb
+++ spec/key_listener_spec.rb
@@ -1,9 +1,14 @@
+# This test fails, even though it excersises the code exactly as I want.
+# There obviosly something about `expect().to receive()` that I don't understand
+#
+# LITTLE HELP?
+#
 require_relative '../lib/key_listener'
 
 describe KeyListener do
   context "calls update proc when key are recived" do
     before do
-      expect(callback).to receive(:call).with(message)
+      expect(callback).to receive(:[]).with(message)
       Thread.new do
         listener.start
       end
@@ -11,7 +16,7 @@ describe KeyListener do
     end
     
     let(:port)     { 2003 }
-    let(:callback) { ->(key){} }
+    let(:callback) { ->(key){} }  # I if you `raise "HI"` in the body of this proc, it's raised (so clearly called)
     let(:listener) { KeyListener.new port, callback }
     let(:message)  { "j" }
     
