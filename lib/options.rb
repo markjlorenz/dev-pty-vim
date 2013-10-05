1318c0e5a2faa7280b7013e697af42a2032f2258
Mark Lorenz <markjlorenz@dapplebeforedawn.com>
2013-10-05 12:38:44 -0400

    make tmp file relative to this repo
    so we can work on file an another directory

--- lib/options.rb
+++ lib/options.rb
@@ -1,8 +1,9 @@
 require 'optparse'
 module Options
   def self.parse!
-    options = OpenStruct.new  key_file: 'tmp/keys', key_port: 2001,
-                     registration_port: 2000,         vim_rc: File.expand_path('~/.vimrc')
+    default_key_file = File.join(__dir__, 'tmp/keys')
+    options = OpenStruct.new  key_file: default_key_file.to_s, key_port: 2001,
+                     registration_port: 2000,                    vim_rc: File.expand_path('~/.vimrc')
 
     OptionParser.new do |opts|
       opts.on("-f", "--key_file=val",           String) { |arg| options.key_file          = arg }
